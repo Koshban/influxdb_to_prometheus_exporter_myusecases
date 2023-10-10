d5263f2c46bedcda1bb81f1eb22e0f7e607b1561
Kaushik Banerjee <kaushik.banerjee77@gmail.com>
2023-10-10 13:08:26 +0800

    using influxdb_client to use url to connect

--- src/query_and_json/getdatainjson.py
+++ src/query_and_json/getdatainjson.py
@@ -1,20 +1,24 @@
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.query_api import QueryApi
 import time
 import json
 import common.connections as connections
 
 # Configure query and execution frequency
-query = "SELECT percentile(\"value\", 95) AS \"95th_percentile\" FROM measurements.gilgo"
+query = 'from(bucket:"measurements") |> range(start: -1h) |> filter(fn: (r) => r._measurement == "gilgo") |> percentile(column: "value", percentile: 0.95)'
 execution_frequency = 900  # 900 seconds = 15 minutes
 
 # Connect to InfluxDB
-client = InfluxDBClient(connections.influxdbconn)
+client = InfluxDBClient(url=connections.influxdbconn, token="your-token", org="your-org")
+
+# Get query client
+query_api = client.query_api()
 
 # Function to execute the query and process the result
 def execute_query():
-    result = client.query(query)
-    # extract the data points and create a JSON object
-    data_points = list(result.get_points())
+    result = query_api.query(query)
+    # Convert the result to a list of dictionaries and create a JSON object
+    data_points = [record.values for table in result for record in table.records]
     json_output = json.dumps(data_points)
     print(json_output)
 
